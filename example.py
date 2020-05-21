c2f24404c0a2bcc0a371960bc7b42f47b572d6a0
masterguo <13913957501@qq.com>
2020-05-21 21:40:38 +0800

    commit v4

---
+++ example.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#coding:utf-8
+def test(**args):
+    return args
+print(test(a=1,b=2))
+
+def test1(a,b,c):
+    return a,b,c
+print(test1(**{'a':1,'b':2,'c':3}))
+print(test1(*[1,2,3]))
---
+++ ping.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#coding:utf-8
+from scapy.all import *
+def ping(a):
+    pkt = IP(dst = a)/ICMP()
+    return sr1(pkt,timeout=2,verbose=False)
+k = input('input a ip address for ping: ')
+
+if ping(k):
+    print('it\'s ok!')
+else:
+    print('it\'s not ok!')
