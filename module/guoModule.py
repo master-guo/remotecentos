47be8dfc65a817aa81516376f13c3d62a6708345
masterguo <13913957501@qq.com>
2020-05-27 21:11:56 +0800

    commit v4

---
+++ module/guoModule.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#coding:utf-8
+a = 100
+b = ['1','a','c']
+def printer(x):
+    print(x)
---
+++ module/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#coding:utf-8
+# from guoModule import printer,a,b
+import guoModule
+guoModule.printer('I\'m guoXiaoGang!')
+guoModule.printer(guoModule.a)
+guoModule.printer(guoModule.b)
