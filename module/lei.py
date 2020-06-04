19eeb38b9453ecff34678f475ab8998443aa519c
masterguo <13913957501@qq.com>
2020-06-04 21:58:39 +0800

    commit v4

---
+++ module/lei.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+#coding:utf-8
+def a(f):
+    return f
+a(5)
--- module/prm.py
+++ module/prm.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 #coding:utf-8
-import paramiko
+import paramiko,os
 def guo_ssh(ip,username,password,port=22,cmd='ls'):
     ssh = paramiko.SSHClient()
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(ip,port=port,username=username,password=password,timeout=5,compress=True)
     stdin,stdout,stderr=ssh.exec_command(cmd)
-    x = stdout.read().decode()
-    return x
+    # x = stdout.read().decode()
+    # return x
+    return stdout.read().decode()
 
 if __name__ == '__main__':
     print(guo_ssh('192.168.18.128','root','redhat',22,'ls'))
+    print(os.getcwd(),os.listdir())
+    f = open(r'test.py','r')
+    print(f.read())
+    f.close()
