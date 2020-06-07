8dd3d8a5de0dd4b4f7475bb2efcaddc319f9ae4a
masterguo <13913957501@qq.com>
2020-06-07 21:25:07 +0800

    commit v4

--- module/lei.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-#coding:utf-8
-def a(f):
-    return f
-a(5)
--- module/xx.py
+++ module/xx.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 #coding:utf-8
-import guoModule
-
-guoModule.printer(guoModule.a)
-guoModule.printer(guoModule.b)
-print(guoModule.__name__)
+from module_ping import *
+from prm import *
+def ping_and_ssh(ip,username,password,cmd):
+    if ping(ip):
+        print("%s is ok."%(ip))
+        print("login"+' '+ip+' '+'now'+':')
+        print(guo_ssh(ip,username,password,22,cmd))
+    else:
+        print("%s is bad." % (ip))
+if __name__ =="__main__":
+    while True:
+        a = input('please input a ip: ')
+        if a != 'quit' or a != 'q':
+            ping_and_ssh(a,username='masterguo',password='redhat',cmd='ls')
